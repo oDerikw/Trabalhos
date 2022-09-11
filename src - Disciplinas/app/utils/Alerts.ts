98a4b1f64eb35cf338d535d2ddfd6568f74f89d4
oDerikw <100731600+oDerikw@users.noreply.github.com>
2022-09-10 23:48:50 -0300

    AppDisciplinas

---
+++ src - Disciplinas/app/utils/Alerts.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+export class Alertas implements OnInit {
+
+  constructor(private alertController: AlertController,
+    private router: Router,
+    private formBuilder:FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  async presentAlert(header: string, subHeader: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      subHeader: subHeader,
+      message: message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertConfirm(header: string, subHeader: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      subHeader: subHeader,
+      message: message,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+          },
+        },
+        {
+          text: 'Confirmar',
+          role: 'confirm',
+          handler: () => {
+            // Coisas aqui
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+}
