472922ea7f2fb77d4a2ae35d086b9ebd7140f48a
oDerikw <100731600+oDerikw@users.noreply.github.com>
2022-10-02 02:45:47 -0300

    dor e sofrimento

---
+++ AppDisciplinas/src/app/services/disciplina-firebase.service.ts
@@ -0,0 +1,111 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Disciplina } from '../models/disciplina';
+import { AngularFireStorage,  } from '@angular/fire/compat/storage';
+import { finalize, take } from 'rxjs/operators';
+import { getStorage, ref, deleteObject, } from "firebase/storage";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DisciplinaFirebaseService {
+  private PATH: string = 'disciplinas';
+  constructor(private angularFirestore: AngularFirestore,
+  private angularFireStorage: AngularFireStorage) { }
+  getDisciplina(id: string) {
+    return this.angularFirestore
+      .collection(this.PATH)
+      .doc(id)
+      .valueChanges();
+  }
+  getDisciplinas() {
+    return this.angularFirestore
+      .collection(this.PATH)
+      .snapshotChanges();
+  }
+  inserirDisciplina(disciplina: Disciplina, id:any) {
+
+    return this.angularFirestore
+      .collection(this.PATH)
+      .doc(id.toString())
+      .set({
+        nome: disciplina.nome,
+        cargaHoraria: disciplina.cargaHoraria,
+        natureza: disciplina.natureza,
+        dataInicio: disciplina.dataInicio,
+        dataFim: disciplina.dataFim,
+        professor: disciplina.professor,
+        vagas: disciplina.vagas,
+        modalidade: disciplina.modalidade,
+        downloadURL: disciplina.downloadURL
+      });
+  }
+  editarDisciplina(disciplina: Disciplina, id: string) {
+    return this.angularFirestore
+      .collection(this.PATH)
+      .doc(id)
+      .update({
+        nome: disciplina.nome,
+        cargaHoraria: disciplina.cargaHoraria,
+        natureza: disciplina.natureza,
+        dataInicio: disciplina.dataInicio,
+        dataFim: disciplina.dataFim,
+        vagas: disciplina.vagas,
+        professor: disciplina.professor,
+        modalidade: disciplina.modalidade, 
+        downloadURL:  disciplina.downloadURL,
+      });
+  }
+  excluirDisciplina(disciplina: Disciplina) {
+    this.excluirImagem(disciplina);
+    return this.angularFirestore
+      .collection(this.PATH)
+      .doc(disciplina.id)
+      .delete();
+  }
+  excluirImagem(disciplina: Disciplina){
+    const path = `images/${disciplina.id}`;
+    return this.angularFireStorage.ref(path).delete();
+  }
+  enviarImagem(imagem: any, disciplina: Disciplina) {
+    let itemId = new Date().getTime();
+    const file = imagem.item(0);
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('Tipo não suportado');
+      return;
+    }
+    const path = `images/${itemId}`;
+    const fileRef = this.angularFireStorage.ref(path);
+    let task = this.angularFireStorage.upload(path, file);
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        let uploadedFileURL = fileRef.getDownloadURL();
+        uploadedFileURL.subscribe((resp) => {
+          disciplina.downloadURL = resp;
+          this.inserirDisciplina(disciplina, itemId);
+        })
+      })
+    ).subscribe();
+    return task;
+  }
+  atualizaImagem(disciplina: Disciplina, id :any, imagem : any) {
+    const file = imagem.item(0);
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('Tipo não suportado');
+      return;
+    }
+    const path = `images/${id}`;
+    const fileRef = this.angularFireStorage.ref(path);
+    let task = this.angularFireStorage.upload(path, file);
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        let uploadedFileURL = fileRef.getDownloadURL();
+        uploadedFileURL.subscribe((resp) => {
+          disciplina.downloadURL = resp;
+          this.editarDisciplina(disciplina, id);
+        })
+      })
+    ).subscribe();
+    return task;
+    }
+}
---
+++ AppDisciplinas/src/environments/environment.prod.ts
@@ -0,0 +1,27 @@
+// -> Derik
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCMLcAaCVKfLINJdU9CHBknzP6DewzKAAI",
+    //authDomain: "appdisciplinas-c13ed.firebaseapp.com",
+    authDomain: "localhost",
+    projectId: "appdisciplinas-c13ed",
+    storageBucket: "appdisciplinas-c13ed.appspot.com",
+    messagingSenderId: "848127633724",
+    appId: "1:848127633724:web:58f08d8cef6760b8f35639"
+  }
+};
+
+/* -> Paula
+export const environment = {
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyBG4quJa7UzhL0Fx3B6XtnqyLNW-JJk22E",
+  authDomain: "appdisciplinas.firebaseapp.com",
+  projectId: "appdisciplinas",
+  storageBucket: "appdisciplinas.appspot.com",
+  messagingSenderId: "100300043722",
+  appId: "1:100300043722:web:f77d826eafd01015bacb33"
+  }
+};
+*/
